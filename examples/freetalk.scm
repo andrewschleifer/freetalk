8998270872d02898f3c308734454228e98423aaa
Anand Babu <ab@zresearch.com>
2007-11-09 00:50:35 +0000

    NEWS, INSTALL, README, freetalk.scm updated.
    git-archimport-id: freetalk@sv.gnu.org/freetalk--main--3.0--patch-9

--- examples/freetalk.scm
+++ examples/freetalk.scm
@@ -1,27 +1,27 @@
 ;; Sample ~/.freetalk/freetalk.scm
-;;
-;; It sets connection parameters and tries to connect on
-;; starting freetalk
 
+;; Passwordless automatic login
 ; (and (string=? (ft-get-jid) "")
 ;      (ft-set-jid! "anand.avati@gmail.com")
 ;      (ft-set-password! "p4ssw0rd")
 ;      (ft-set-sslconn! #t)
 ;      (ft-set-prompt! "freetalk: ")
 ;      (ft-set-server! "talk.google.com"))
-; 
+
+;; Get control after successful login
 ; (add-hook! ft-login-hook
 ;            (lambda (status)
 ;              (if status
 ;                  (begin
+                     ;; Set user-id as your prompt                   
 ;                    (ft-set-prompt! (string-append 
 ;                                     (car (string-split (ft-get-jid) #\@))
 ;                                     ": "))
-;                    (ft-set-status-msg! "online")))))
+                     ;; Change status to "do not disturb"
+;                    (ft-set-status-msg! "dnd")))))
 
-;;;
-;;; Let ctrl-a display full roster, ctrl-e who i am
+
+;; Key bindings
+;; Let ctrl-a display full roster, ctrl-e who i am
 ; (ft-bind-to-ctrl-key #\a "(/who \"all\")")
 ; (ft-bind-to-ctrl-key #\e "(/whoami \"\")")
-;;; ctrl-i - send a hug to your mother with a press of a button
-; (ft-bind-to-ctrl-key #\h "(ft-send-message \"mymother@gmail.com\" \">:D<\")")
